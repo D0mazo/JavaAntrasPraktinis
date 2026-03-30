b22a80d431948c3d14ed5b75fa278030789db05a
D0mazo <dominykas.simanavicius@proton.me>
2026-03-30 12:23:37 +0300

    first commit

---
+++ src/test/java/viko/eif/lt/simanaviciusd/PI24SN/task2/Task2ApplicationTests.java
@@ -0,0 +1,13 @@
+package viko.eif.lt.simanaviciusd.PI24SN.task2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
